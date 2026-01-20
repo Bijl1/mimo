e2c5fc50306cc3139416896e3d29b4432b83169d
jonathan <sannoayo@gmail.com>
2026-01-20 16:49:23 -0500

    Add basic variable examples with types, concatenation, and comparisons

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 var age = 100;
 console.log(age);
+
+var age = 500;
+console.log("Age: " + age);
+
+const sixtyNine = 69;
+console.log(sixtyNine + " new messages");
+
+const dji = true;
+const text = "Did DJI get the FCC covered list on Dec 23: "
+console.log(text + dji);
+console.log(`${text}${dji}`);
+
+const number = 91;
+const winner = "Lucky number: " + number;
+console.log(winner);
+
+const fish = 10;
+var see = fish + " fish";
+console.log(see);
+
+var see = fish + (fish === 1 ? " fish" : " fish");
+console.log(see);
+
+const movie = "Joker";
+const score = 90;
+console.log(movie + " audience score: " + score);
+
+const user = "John";
+const isOnline = true;
+const message = user + " is online: " + isOnline;
+console.log(message);
